c31358656e8fb875fc3048169bfb4c29d05f3162
Jonathan J. Lee <JJL@MacBook-Pro.local>
2017-02-27 12:28:43 -0500

    Initial Commit

---
+++ Ecolo Model Testing/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Ecolo Model Testing
+//
+//  Created by Jonathan J. Lee on 2/27/17.
+//  Copyright © 2017 Jonathan J. Lee. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
